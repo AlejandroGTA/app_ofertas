432d8dcb29022d5eb3f4f39935f8e6034bb49a51
AlejandroGTA <36650624+AlejandroGTA@users.noreply.github.com>
2020-06-13 12:53:33 -0500

    update port dinamyc

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.engine('ejs',engine);
 app.set('view engine','ejs');
 app.use('/public',express.static('public'));
 
-const port = 5000;
+const port = process.env.port || 5000;
 
 app.listen(port,function (){
     console.log("Started");
