976b94ed6ae7225bd2419220664750880646a6ee
AlejandroGTA <36650624+AlejandroGTA@users.noreply.github.com>
2020-06-13 14:06:47 -0500

    Up file port

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.engine('ejs',engine);
 app.set('view engine','ejs');
 app.use('/public',express.static('public'));
 
-const port = process.env.port || 5000;
+const port = process.env.port || 8080;
 
 app.listen(port,function (){
     console.log("Started");
